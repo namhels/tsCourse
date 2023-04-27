08513e412db1e32f99043eedd322b69a9640dfbe
namhels <8.lsv.888@gmail.com>
2023-04-27 09:42:50 +0300

    add classes in practiceVM

--- dist/practiceVM.js
+++ dist/practiceVM.js
@@ -1,4 +1,46 @@
 "use strict";
+class Typescript {
+    constructor(version) {
+        this.version = version;
+    }
+    info(name) {
+        return `[${name}]: Typescript version is ${this.version}`;
+    }
+}
+class Car {
+    constructor(model) {
+        this.model = model;
+        this.numberOfWheels = 4;
+    }
+}
+class Animal {
+    constructor() {
+        this.voice = '';
+        this.color = 'black';
+        this.go();
+    }
+    go() {
+        console.log('Go');
+    }
+}
+class Cat extends Animal {
+    setVoice(voice) {
+        this.voice = voice;
+    }
+}
+const cat = new Cat();
+cat.setVoice('test');
+console.log(cat.color);
+class ComponentGeneral {
+}
+class AppComponent extends ComponentGeneral {
+    render() {
+        console.log('Component on render');
+    }
+    info() {
+        return 'This is info';
+    }
+}
 function strip(x) {
     if (typeof x === 'number') {
         return x.toFixed(2);
--- src/practiceVM.ts
+++ src/practiceVM.ts
@@ -1,3 +1,74 @@
+// ======  classes  =========
+
+class Typescript {
+  version: string
+
+  constructor(version: string) {
+    this.version = version
+  }
+
+  info(name: string) {
+    return `[${name}]: Typescript version is ${this.version}`
+  }
+}
+
+// class Car {
+//   readonly model: string
+//   readonly numberOfWheels: number = 4
+//
+//   constructor(theModel: string) {
+//     this.model = theModel
+//   }
+// }
+
+class Car {
+  readonly numberOfWheels: number = 4
+  constructor(readonly model: string) {}
+}
+// ==============
+
+
+class Animal {
+  protected voice: string = ''
+  public color: string = 'black'
+
+  constructor() {
+    this.go()
+  }
+
+  private go() {
+    console.log('Go')
+  }
+}
+
+class Cat extends Animal {
+  public setVoice(voice: string): void {
+    this.voice = voice
+  }
+}
+
+const cat = new Cat()
+cat.setVoice('test')
+console.log(cat.color)
+// cat.voice
+
+// =====================
+
+abstract class ComponentGeneral {
+  abstract render(): void
+  abstract info(): string
+}
+
+class AppComponent extends ComponentGeneral {
+  render(): void {
+    console.log('Component on render')
+  }
+
+  info(): string {
+    return 'This is info';
+  }
+}
+
 // ======  guards  =========
 
 function strip(x: string | number) {
