2ec5f14e1c020b19b53e1796c459ca18b7b152b0
namhels <8.lsv.888@gmail.com>
2023-05-01 20:09:16 +0300

    add interfaces

--- dist/practiceVM.js
+++ dist/practiceVM.js
@@ -1,4 +1,45 @@
 "use strict";
+const rect1 = {
+    id: '1234',
+    size: {
+        width: 20,
+        height: 30
+    },
+    color: '#ccc'
+};
+const rect2 = {
+    id: '12345',
+    size: {
+        width: 10,
+        height: 5
+    }
+};
+rect2.color = 'black';
+const rect3 = {};
+const rect4 = {};
+const rect5 = {
+    id: '123',
+    size: {
+        width: 20,
+        height: 20
+    },
+    getArea() {
+        return this.size.width * this.size.height;
+    }
+};
+class Clock {
+    constructor() {
+        this.time = new Date();
+    }
+    setTime(date) {
+        this.time = date;
+    }
+}
+const css = {
+    border: '1px solid black',
+    marginTop: '2px',
+    borderRadius: '5px'
+};
 var Membership;
 (function (Membership) {
     Membership[Membership["Simple"] = 0] = "Simple";
