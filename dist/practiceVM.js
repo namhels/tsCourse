1b112446fb6a8ef378ec24929934fd4d71930d67
namhels <8.lsv.888@gmail.com>
2023-05-06 21:43:16 +0300

    add types to practiceVM

--- dist/practiceVM.js
+++ dist/practiceVM.js
@@ -1,4 +1,31 @@
 "use strict";
+const isFetching = true;
+const isLoading = false;
+const int = 42;
+const float = 4.2;
+const num = 3e10;
+const message = 'Hello Typescript';
+const numberArray = [1, 1, 2, 3, 5, 8, 13];
+const numberArray2 = [1, 1, 2, 3, 5, 8, 13];
+const words = ['Hello', 'Typescript'];
+const contact = ['Vladilen', 1234567];
+let variable = 42;
+variable = 'New String';
+variable = [];
+function sayMyName(name) {
+    console.log(name);
+}
+sayMyName('Хайзенберг');
+function throwError(message) {
+    throw new Error(message);
+}
+function infinite() {
+    while (true) {
+    }
+}
+const login = 'admin';
+const id1 = 1234;
+const id2 = '1234';
 const rect1 = {
     id: '1234',
     size: {
